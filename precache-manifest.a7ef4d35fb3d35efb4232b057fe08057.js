5e9547136a0c6ee685da94799f2458803eba620e
Abhi1108 <ahdoshi.1108@gmail.com>
2018-11-01 20:21:03 +0530

    Updates

---
+++ precache-manifest.a7ef4d35fb3d35efb4232b057fe08057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9613b1bf75bd1565ad60",
+    "url": "/contactmanager/static/js/main.9613b1bf.chunk.js"
+  },
+  {
+    "revision": "13a1d49d30e515474409",
+    "url": "/contactmanager/static/js/1.13a1d49d.chunk.js"
+  },
+  {
+    "revision": "9613b1bf75bd1565ad60",
+    "url": "/contactmanager/static/css/main.09e997af.chunk.css"
+  },
+  {
+    "revision": "13a1d49d30e515474409",
+    "url": "/contactmanager/static/css/1.0e5eddbf.chunk.css"
+  },
+  {
+    "revision": "94210c91ee43808425aef880c969ed34",
+    "url": "/contactmanager/index.html"
+  }
+];
